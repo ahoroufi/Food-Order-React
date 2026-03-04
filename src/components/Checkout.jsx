0a92513fd9c1df63d141ef1b33fa759d60d7ab67
Arash Horoufi <6829077+ahoroufi@users.noreply.github.com>
2026-03-03 17:33:45 -0800

    296. Handling From Submission & validation

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -19,9 +19,19 @@ export default function Checkout() {
     userProgressCtx.hideCheckout()
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target)
+//  object with the input prperties and values entered
+//  { email: text@example.com}
+    const customerData = Object.fromEntries(fd.entries()); 
+                                                            
+  }
+
   return (
     <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
